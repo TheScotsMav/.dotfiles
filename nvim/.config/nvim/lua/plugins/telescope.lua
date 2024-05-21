9ef451b3bcdd840467963c32684a91bc0045dbe0
TheScotsMav <thrill.seeking.maverick@gmail.com>
2024-05-21 19:22:02 +0100

    update telescope to lazy bindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -14,17 +14,21 @@ return {
           },
         },
       })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set("n", "<leader>ps", function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      end)
-      vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-
       require("telescope").load_extension("ui-select")
     end,
+    keys = {
+      { "<leader>ff",       require("telescope.builtin").find_files, desc = "" },
+      { "<C-p>",            require("telescope.builtin").git_files,  desc = "" },
+      { "<leader>fg",       require("telescope.builtin").live_grep,  desc = "" },
+      { "<leader><leader>", require("telescope.builtin").oldfiles,   desc = "" },
+      { "<leader>vh",       require("telescope.builtin").help_tags,  desc = "" },
+      {
+        "<leader>ps",
+        function()
+          require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+        end,
+        desc = "",
+      },
+    },
   },
 }
